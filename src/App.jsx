ffee83c93e4c50f9091ac27635b3ffc230ec94ef
Pritam Ghosh <44809987+meghoshpritam@users.noreply.github.com>
2024-05-05 14:16:41 +0530

    chore: Refactor code to implement lazy loading for job images

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,53 @@ import { useDispatch } from 'react-redux';
 import { getJobs } from '@/services/job.service';
 import { stopLoading, startLoading, addJobs } from '@/reducer/job';
 import JobsContainer from '@/components/JobsContainer';
+import reduxStore from './store';
+
+const JOB_LIMIT_PER_REQUEST = 12;
 
 function App() {
   const dispatch = useDispatch();
 
-  useEffect(() => {
+  const fetchJobs = async (offset = 0) => {
     dispatch(startLoading());
-    getJobs({
-      limit: 12,
-      offset: 0,
-    })
-      .then((data) => {
-        dispatch(
-          addJobs({
-            jobs: data.jdList,
-            availableNumberOfJobs: data.totalCount,
-          }),
-        );
-      })
-      .catch((error) => {})
-      .finally(() => {
-        dispatch(stopLoading());
+    try {
+      const jobData = await getJobs({
+        limit: JOB_LIMIT_PER_REQUEST,
+        offset,
       });
+
+      dispatch(
+        addJobs({
+          jobs: jobData.jdList,
+          availableNumberOfJobs: jobData.totalCount,
+        }),
+      );
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    } finally {
+      dispatch(stopLoading());
+    }
+  };
+
+  const onScrollLoadJobs = async () => {
+    const jobDetails = reduxStore.getState().job;
+
+    if (document.documentElement.offsetHeight - (window.innerHeight + document.documentElement.scrollTop) > 15) return;
+
+    if (!jobDetails.loading) {
+      await fetchJobs(jobDetails.jobs.length);
+    }
+
+    if (jobDetails.jobs.length >= jobDetails.availableNumberOfJobs) {
+      window.removeEventListener('scroll', onScrollLoadJobs);
+    }
+  };
+
+  useEffect(() => {
+    fetchJobs();
+
+    window.addEventListener('scroll', onScrollLoadJobs);
+    return () => window.removeEventListener('scroll', onScrollLoadJobs);
   }, []);
 
   return (
--- src/components/JobsContainer.jsx
+++ src/components/JobsContainer.jsx
@@ -12,51 +12,50 @@ function JobsContainer() {
   const jobDetails = useSelector(jobState);
   console.log('📢[JobsContainer.jsx:7]: job: ', jobDetails);
 
-  if (jobDetails.loading) {
-    return <div>Loading...</div>;
-  }
-
   return (
-    <Grid container spacing={5}>
-      {jobDetails.jobs.map((job) => (
-        <Grid item xs={12} sm={6} md={4} lg={3} key={job.jdUid}>
-          <Card sx={{ minWidth: 275 }}>
-            <CardContent>
-              <Grid container>
-                <Grid item sm={4}>
-                  <img src={job.logoUrl} alt={job.companyName} title={job.companyName} width="100" height="100" />
+    <>
+      <Grid container spacing={5}>
+        {jobDetails.jobs.map((job) => (
+          <Grid item xs={12} sm={6} md={4} lg={3} key={job.jdUid}>
+            <Card sx={{ minWidth: 275 }}>
+              <CardContent>
+                <Grid container>
+                  <Grid item sm={4}>
+                    <img src={job.logoUrl} alt={job.companyName} title={job.companyName} width="100" height="100" />
+                  </Grid>
+                  <Grid item sm={8}>
+                    <Typography sx={{ fontSize: 14 }} gutterBottom>
+                      {job.companyName}
+                    </Typography>
+                    <Typography sx={{ fontSize: 14 }} gutterBottom>
+                      {job.jobRole}
+                    </Typography>
+                    <Typography sx={{ fontSize: 14 }} gutterBottom>
+                      {job.location}
+                    </Typography>
+                  </Grid>
                 </Grid>
-                <Grid item sm={8}>
-                  <Typography sx={{ fontSize: 14 }} gutterBottom>
-                    {job.companyName}
-                  </Typography>
-                  <Typography sx={{ fontSize: 14 }} gutterBottom>
-                    {job.jobRole}
-                  </Typography>
-                  <Typography sx={{ fontSize: 14 }} gutterBottom>
-                    {job.location}
-                  </Typography>
-                </Grid>
-              </Grid>
-              <Typography sx={{ fontSize: 14 }} gutterBottom>
-                Estimated Salary: {job.minBasePay} - {job.maxBasePay}
-              </Typography>
-              <Typography sx={{ fontSize: 14 }} gutterBottom>
-                About Company:
-                <br />
-                {job.jobDetailsFromCompany}
-              </Typography>
-              <Typography sx={{ fontSize: 14 }} gutterBottom>
-                Minimum Experience: {job.minExp} - {job.maxExp} years
-              </Typography>
-            </CardContent>
-            {/* <CardActions>
+                <Typography sx={{ fontSize: 14 }} gutterBottom>
+                  Estimated Salary: {job.minBasePay} - {job.maxBasePay}
+                </Typography>
+                <Typography sx={{ fontSize: 14 }} gutterBottom>
+                  About Company:
+                  <br />
+                  {job.jobDetailsFromCompany}
+                </Typography>
+                <Typography sx={{ fontSize: 14 }} gutterBottom>
+                  Minimum Experience: {job.minExp} - {job.maxExp} years
+                </Typography>
+              </CardContent>
+              {/* <CardActions>
               <Button size="small">Learn More</Button>
             </CardActions> */}
-          </Card>
-        </Grid>
-      ))}
-    </Grid>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+      {jobDetails.loading && <h1>Loading...</h1>}
+    </>
   );
 }
 
